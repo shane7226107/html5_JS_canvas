c3cdeffbbf7e4a8c74675d94655742613069c80d
Shane Liu <shane7226107@gmail.com>
2013-12-16 11:01:38 +0800

    creat a node app

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.get('*', function (req, res) {
+  console.log(req.params[0]);
+  res.sendfile(__dirname + '/public/'+req.params[0]);
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
